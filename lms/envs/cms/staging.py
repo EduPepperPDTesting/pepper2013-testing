34c6d31c45b3e80f8f2e4ed5d44226e059ec24fc
victorfence <victor@education2000.com>
2013-12-31 10:54:57 +0000

    Prepare for gunicron

---
+++ lms/envs/cms/staging.py
@@ -0,0 +1,64 @@
+from ..dev import *
+
+# ========================================================================
+# ========================================================================
+
+MITX_FEATURES['ENABLE_SQL_TRACKING_LOGS'] = False
+
+# ========================================================================
+# ========================================================================
+
+MITX_FEATURES['AUTH_USE_MIT_CERTIFICATES'] = False
+
+SUBDOMAIN_BRANDING['edge'] = 'edge'
+SUBDOMAIN_BRANDING['preview.edge'] = 'edge'
+VIRTUAL_UNIVERSITIES = ['edge']
+
+# Turn off this flag because it will render 'Edit / QA' links for all instructor viewings of
+# modules. Since - for now - those links point to github (for XML based authoring), it seems broken
+# to people using it. Once we can update those links to properly link back to Studio,
+# then we can turn this flag back on, as well as enabling in aws.py configurations.
+MITX_FEATURES['ENABLE_LMS_MIGRATION'] = False
+
+META_UNIVERSITIES = {}
+
+modulestore_options = {
+    'default_class': 'xmodule.raw_module.RawDescriptor',
+    'host': 'localhost',
+    'db': 'xmodule',
+    'collection': 'modulestore',
+    'fs_root': DATA_DIR,
+    'render_template': 'mitxmako.shortcuts.render_to_string',
+}
+
+MODULESTORE = {
+    'default': {
+        'ENGINE': 'xmodule.modulestore.mongo.MongoModuleStore',
+        'OPTIONS': modulestore_options
+    },
+}
+
+CONTENTSTORE = {
+    'ENGINE': 'xmodule.contentstore.mongo.MongoContentStore',
+    'OPTIONS': {
+        'host': 'localhost',
+        'db': 'xcontent',
+    }
+}
+
+INSTALLED_APPS += (
+    # Mongo perf stats
+    'debug_toolbar_mongo',
+    )
+
+DEBUG_TOOLBAR_PANELS += (
+   'debug_toolbar_mongo.panel.MongoDebugPanel',
+   )
+
+# ========================================================================
+# ========================================================================
+# ========================================================================
+# ========================================================================
+# ========================================================================
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
