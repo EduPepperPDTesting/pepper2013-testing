c44b3da10b4ff20365efebdb703dde9e076775af
victorfence <victor@education2000.com>
2014-03-12 13:45:58 +0000

    Expire after 30min of inactive session. Modify invite email content and subject.

---
+++ lms/djangoapps/middleware.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+
+class SessionExpiry(object):
+    """ Set the session expiry according to settings """
+    def process_request(self, request):
+        if getattr(settings, 'SESSION_EXPIRY', None):
+            request.session.set_expiry(settings.SESSION_EXPIRY)
+        return None
+
+
+    
