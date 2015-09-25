a202d8e3dc406151cdb0f7e38fbb5363693f7b02
Jim Baker <jbaker@pcgus.com>
2015-09-25 12:05:05 -0400

    Took out jQuery functionality.

--- common/lib/xmodule/xmodule/js/src/html/edivate.js
+++ common/lib/xmodule/xmodule/js/src/html/edivate.js
@@ -1,3 +1,11 @@
+function appendHtml(el, str) {
+    var div = document.createElement('div');
+    div.innerHTML = str;
+    while (div.children.length > 0) {
+        el.appendChild(div.children[0]);
+    }
+}
+
 function EmbedEdivate() {}
 
 EmbedEdivate.prototype.embed = function(element_name, assest_id) {
@@ -5,12 +13,17 @@ EmbedEdivate.prototype.embed = function(element_name, assest_id) {
     var lastName = '';
     var email = '';
     var secret = '';
-    $.get("/user-info", function(data) {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', '/user-info', false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200) {
+        var data = eval("(" + xmlhttp.responseText + ")");
         firstName = data.first_name;
         lastName = data.last_name;
         email = data.email;
         secret = data.secret;
-    });
+    }
     if (email && secret) {
         var hash = md5(email + secret);
         var embed_code = '<iframe frameborder="0" scrolling="no" width="800" height="450" src="https://www.pd360.com/partners/EmbeddedPlayer/player.cfm?ncesid=12345&group=MantecaUnified&email=';
@@ -24,6 +37,7 @@ EmbedEdivate.prototype.embed = function(element_name, assest_id) {
         embed_code += '&contentid=';
         embed_code += assest_id;
         embed_code += '"></iframe>';
-        $('#' + element_name).append(embed_code);
+
+        appendHtml(document.getElementById(element_name), embed_code);
     }
 };
