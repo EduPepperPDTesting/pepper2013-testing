5465ff2ff346efcb4ef71469febf73a494ae4dc3
Jim Baker <jbaker@pcgus.com>
2015-10-20 10:00:26 -0400

    Code stub for TNL integration.

---
+++ lms/djangoapps/tnl_integration/models.py
@@ -0,0 +1,22 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TNLCourses(models.Model):
+    class Meta:
+        db_table = 'tnl_courses'
+    course = models.CharField(blank=False, max_length=255, db_index=True)
+    registered = models.BooleanField(blank=False, default=0)
+    date_added = models.DateTimeField(auto_now_add=True, db_index=False)
+    registration_date = models.DateTimeField(blank=True, db_index=False)
+
+
+class TNLCompletionTrack(models.Model):
+    class Meta:
+        db_table = 'tnl_completion_track'
+    user = models.ForeignKey(User, default=0)
+    course = models.ForeignKey(TNLCourses)
+    registered = models.BooleanField(blank=False, default=0)
+    date_added = models.DateTimeField(auto_now_add=True, db_index=False)
+    registration_date = models.DateTimeField(blank=True, db_index=False)
---
+++ lms/djangoapps/tnl_integration/views.py
@@ -0,0 +1,17 @@
+def register_course():
+    """
+    This registers the course with TNL
+    """
+
+
+def register_completion():
+    """
+    This registers a competed course for a particular user with TNL
+    """
+
+
+def check_completions():
+    """
+    This goes through
+    :return:
+    """
