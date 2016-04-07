92b5e99afd06f316423f277c866376f508e817f9
victorfence <victor@education2000.com>
2016-04-07 15:24:20 +0800

    Try to add online status.

--- lms/djangoapps/middleware.py
+++ lms/djangoapps/middleware.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.utils import timezone
+from student.models import UserProfile
 
 class SessionExpiry(object):
     """ Set the session expiry according to settings """
@@ -8,4 +10,9 @@ class SessionExpiry(object):
         return None
 
 
-    
+class UpdateLastActivityMiddleware(object):
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        assert hasattr(request, 'user'), 'The UpdateLastActivityMiddleware requires authentication middleware to be installed.'
+        if request.user.is_authenticated():
+            UserProfile.objects.filter(user__id=request.user.id) \
+                           .update(last_activity=timezone.now())
