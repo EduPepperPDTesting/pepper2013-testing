c7200cd8ce37a487779a41d6c1dfa616940cc654
Anthony.Lu <Anthony.Lu@education2000.com>
2016-08-09 14:56:04 +0800

    Modify the style of ourteam 20160809.

---
+++ lms/static/js/script_jcarousellite.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".main .jCarouselLite").jCarouselLite({
+        btnNext: ".main .next",
+        btnPrev: ".main .prev",
+        speed: 500,
+        easing: "easeinout",
+       	auto: 4000
+    });
+});
+
