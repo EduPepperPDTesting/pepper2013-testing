82c1c63f1a39ce2ce3e80dfc499d2af0e38d660a
Anatoliy <Anatoliy.kogan@gmail.com>
2017-03-30 11:49:42 -0400

    day calendar test

--- lms/djangoapps/administration/pepreg.py
+++ lms/djangoapps/administration/pepreg.py
@@ -801,7 +801,7 @@ def build_week_rows(year, month, catype, all_occurrences, current_day, tmp_schoo
                             table_tr_content += "</div>"
 
                             if (day[3][0] != dayHour or h > endHour ):
-                                table_tr_content += "<div class='training-row' style='display: block; width: 100%; box-sizing: border-box; padding: 5px; border-bottom: 1px solid #ccc; text-align: right;' id='" + dayHour + "'>&nbsp;"+str(h)
+                                table_tr_content += "<div class='training-row' style='display: block; width: 100%; box-sizing: border-box; padding: 5px; border-bottom: 1px solid #ccc; text-align: right;' id='" + dayHour + "'>&nbsp;"
 
                         table_tr_content += "</div>"
 
