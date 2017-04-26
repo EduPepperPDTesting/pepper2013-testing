fd8683f34fc74f57a5b4751282ebcfa9f4b8277d
Anthony.Lu <Anthony.Lu@education2000.com>
2017-04-26 14:04:09 +0800

    edit my chunks

--- lms/djangoapps/notifications/views.py
+++ lms/djangoapps/notifications/views.py
@@ -68,14 +68,6 @@ def save_interactive_update(request):
     oid = getObjectId()
     info['_id']=oid
     save_interactive_info(info)
-
-    ma_db = myactivitystore()    
-    my_activity = {"GroupType": "MyChunks", "EventType": "myChunks_editChunkinMyChunksPage", "ActivityDateTime": datetime.utcnow(), "UsrCre": request.user.id, 
-    "URLValues": {"url": info['location']},
-    "TokenValues": {"UsrCre": request.user.id, "url": info['location']}, #"TokenValues": {"user_id":info['user_id'], "SourceID": oid}, 
-    "LogoValues": {"SourceID": oid}}    
-    ma_db.insert_item(my_activity)
-
     return utils.JsonResponse({'results':'true'})
 
 def set_interactive_update(request):
