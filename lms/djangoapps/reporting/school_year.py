0a4c43ae4dd93537f2b9ca7f6b345fa342813d0d
sj <495842314@qq.com>
2017-05-02 14:10:04 +0800

    return

--- lms/djangoapps/reporting/school_year.py
+++ lms/djangoapps/reporting/school_year.py
@@ -2,7 +2,7 @@
 from .models import ReportViews
 from .models import reporting_store
 
-school_year_collection = ['UserView', 'UserCourseView', 'PepRegTrainingView']
+school_year_collection = ['UserView', 'UserCourseView', 'PDPlannerView']
 
 
 def report_has_school_year(columns):
