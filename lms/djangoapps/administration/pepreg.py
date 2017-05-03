984e4716482d52b568d1a299a19c2d702f3c99b8
Anatoliy <anatoliy.kogan@gmail.com>
2017-05-02 20:32:50 -0400

    pdf print update

--- lms/djangoapps/administration/pepreg.py
+++ lms/djangoapps/administration/pepreg.py
@@ -1259,7 +1259,7 @@ def download_calendar_pdf(request):
                     if(not after_long_text):
                         ty -= 10 * num_lines
                     else:
-                        ty -= 10
+                        ty += 10
 
                     after_long_text = 1
 
