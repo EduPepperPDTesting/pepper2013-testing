7022a5588f8de63651475589aa975825a460477a
Anatoliy <anatoliy.kogan@gmail.com>
2017-05-30 11:56:22 -0400

    calendar pdf print update

--- lms/djangoapps/administration/pepreg.py
+++ lms/djangoapps/administration/pepreg.py
@@ -1364,17 +1364,17 @@ def download_calendar_pdf(request):
                 training = PepRegTraining.objects.get(id=training_id)
 
                 try:
-                    district = District.objects.get(id=training.district)
-                    dist_name = district.name
-                #     try:
-                #         dist_logo = ImageReader("https://" + request.get_host() + '/static/images/' + dist_name + '.jpg')
-                #         c.drawImage(dist_logo, 30, 750, 200, 73)
-                #     except:
-                #         try:
-                #             dist_logo = ImageReader("http://" + request.get_host() + '/static/images/' + dist_name + '.jpg')
-                #             c.drawImage(dist_logo, 30, 750, 200, 73)
-                #         except:
-                #             console.log('no logo')
+                    # district = District.objects.get(id=training.district)
+                    dist_name = training.district.name
+                    try:
+                        dist_logo = ImageReader("https://" + request.get_host() + '/static/images/' + dist_name + '.jpg')
+                        c.drawImage(dist_logo, 30, 750, 200, 73)
+                    except:
+                        try:
+                            dist_logo = ImageReader("http://" + request.get_host() + '/static/images/' + dist_name + '.jpg')
+                            c.drawImage(dist_logo, 30, 750, 200, 73)
+                        except:
+                            console.log('no logo')
                 except:
                     console.log("couldn't load logo")
 
