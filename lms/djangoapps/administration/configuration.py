d00f5eabcc4507d02d5e45927a2cff2b49973653
sj <495842314@qq.com>
2017-06-30 09:38:49 +0800

    test

--- lms/djangoapps/administration/configuration.py
+++ lms/djangoapps/administration/configuration.py
@@ -399,7 +399,7 @@ def backup_training_student(year):
     for train in trainings:
         training_backup = PepRegStudent_Backup()
         training_backup.school_year = year
-        training_backup.training =train.training.id
+        training_backup.training =train.training
         training_backup.student=train.student
         training_backup.student_status=train.student_status
         training_backup.student_credit=train.student_credit
@@ -414,7 +414,7 @@ def backup_training_instructor(year):
     for train in trainings:
         training_backup = PepRegInstructor_Backup()
         training_backup.school_year = year
-        training_backup.training =train.training.id
+        training_backup.training =train.training
         training_backup.instructor=train.instructor
         training_backup.user_create=train.user_create
         training_backup.date_create=train.date_create
--- lms/djangoapps/administration/models.py
+++ lms/djangoapps/administration/models.py
@@ -266,7 +266,6 @@ class UserLoginInfo(models.Model):
 class PepRegTraining_Backup(models.Model):
     class Meta:
         db_table = 'pepreg_training_hist'
-    id = models.IntegerField(primary_key=True)
     type = models.CharField(blank=False, max_length=50, db_index=False)
     district = models.ForeignKey(District)
     description = models.TextField(blank=False, null=True)
@@ -298,7 +297,7 @@ class PepRegTraining_Backup(models.Model):
 class PepRegInstructor_Backup(models.Model):
     class Meta:
         db_table = 'pepreg_instructor_hist'
-    training = models.IntegerField(blank=False)
+    training = models.ForeignKey(PepRegTraining_Backup)
     instructor = models.ForeignKey(User, related_name='+')
     user_create = models.ForeignKey(User, related_name='+')
     date_create = models.DateField(auto_now_add=True, db_index=False)
@@ -310,7 +309,7 @@ class PepRegInstructor_Backup(models.Model):
 class PepRegStudent_Backup(models.Model):
     class Meta:
         db_table = 'pepreg_student_hist'
-    training = models.IntegerField(blank=False)
+    training = models.ForeignKey(PepRegTraining_Backup)
     student = models.ForeignKey(User, related_name='+')
     student_status = models.CharField(blank=False, max_length=50, db_index=False)
     student_credit = models.FloatField(blank=False, default=0)
