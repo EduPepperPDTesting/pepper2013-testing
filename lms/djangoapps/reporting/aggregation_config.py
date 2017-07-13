aacca9e2fb9b0a59b06e4fc16a531d2aedeaacc9
sj <495842314@qq.com>
2017-07-13 11:10:23 +0800

    test

--- lms/djangoapps/reporting/aggregation_config.py
+++ lms/djangoapps/reporting/aggregation_config.py
@@ -1988,10 +1988,10 @@ get_create_int_row_headers = '''
 get_create_collection_tmp = '''{
     "$group":{
         "_id":{"{column_data}":"${column_data}",'{row_data}':"${row_data}"},
-        'max':{"$max":"{aggregate_data}"},
-        'min':{"$min":"{aggregate_data}"},
-        'avg':{"$avg":"{aggregate_data}"},
-        'total':{"$sum":"{aggregate_data}"},
+        'max':{"$max":"${aggregate_data}"},
+        'min':{"$min":"${aggregate_data}"},
+        'avg':{"$avg":"${aggregate_data}"},
+        'total':{"$sum":"${aggregate_data}"},
         "count":{"$sum":1}
         }
     },
