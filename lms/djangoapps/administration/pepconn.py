f458da4c3f20b09775ee1995f090a0beb54c6466
scott <495842314@qq.com>
2017-08-14 10:49:32 +0800

    change createuser enroll crouse

--- lms/djangoapps/administration/pepconn.py
+++ lms/djangoapps/administration/pepconn.py
@@ -1191,7 +1191,10 @@ def single_user_submit(request):
         profile.subscription_status = "Imported"
 
         #** course enroll
-        cea, _ = CourseEnrollmentAllowed.objects.get_or_create(course_id='PCG_Education/PEP101.1/S2016', email=email)
+        if district.code == "3968593":
+            cea, _ = CourseEnrollmentAllowed.objects.get_or_create(course_id='PCG_Education/PEP101.2/F2017', email=email)
+        else:
+            cea, _ = CourseEnrollmentAllowed.objects.get_or_create(course_id='PCG_Education/PEP101.1/S2016', email=email)
         cea.is_active = True
         cea.auto_enroll = True
         cea.save()
