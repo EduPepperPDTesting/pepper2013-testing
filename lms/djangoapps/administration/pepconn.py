8bae2e0a4bb59067e4e6b7a8b4358e8587296cb4
sj <495842314@qq.com>
2017-08-14 10:54:02 +0800

    change createuser enroll course

--- lms/djangoapps/administration/pepconn.py
+++ lms/djangoapps/administration/pepconn.py
@@ -1058,7 +1058,10 @@ def do_import_user(task, csv_lines, request):
             profile.subscription_status = "Imported"
 
             #** course enroll
-            cea, _ = CourseEnrollmentAllowed.objects.get_or_create(course_id='PCG_Education/PEP101.1/S2016', email=email)
+            if district.code == "3968593":
+                cea, _ = CourseEnrollmentAllowed.objects.get_or_create(course_id='Edu2000/EDU101/2014_EDU_Jan', email=email)
+            else:
+                cea, _ = CourseEnrollmentAllowed.objects.get_or_create(course_id='PCG_Education/PEP101.1/S2016', email=email)
             cea.is_active = True
             cea.auto_enroll = True
             cea.save()
