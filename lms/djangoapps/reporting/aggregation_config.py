12d610be7408287fb179a673a246d6579607e91f
sj <495842314@qq.com>
2017-09-14 09:01:36 +0800

    report add firstname and lastname

--- lms/djangoapps/reporting/aggregation_config.py
+++ lms/djangoapps/reporting/aggregation_config.py
@@ -822,7 +822,13 @@ AggregationConfig["PDPlannerView"]["query"] = '''{school_year}{pd_domain}{
         },
         "user_school": {
             "$arrayElemAt": ["$user_info.school", 0]
-        }
+        },
+        "first_name":{
+            "$arrayElemAt": ["$user_info.first_name", 0]
+        },
+        "last_name":{
+            "$arrayElemAt": ["$user_info.last_name", 0]
+        },
     }
 },{pd_user_domain}{filters}{display_columns}{distinct}'''
 
