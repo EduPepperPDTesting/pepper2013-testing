bcb3fd9c37a1329c139ee4308c22a9cc2adb9c27
Anatoliy <anatoliy.kogan@gmail.com>
2017-09-26 13:01:24 -0400

    The field 'UserLoginInfo.user_id' does not have a default specified, yet is NOT NULL.

--- lms/djangoapps/administration/models.py
+++ lms/djangoapps/administration/models.py
@@ -253,6 +253,7 @@ class UserLoginInfo(models.Model):
     class Meta:
         db_table = 'user_login_info'
     user = models.ForeignKey(UserProfile, to_field='user', on_delete=models.CASCADE, related_name="loginfo")
+    user_id = models.IntegerField(blank=False, max_length=11, default=0)
     login_time = models.CharField(max_length=30)
     logout_time = models.CharField(max_length=30)
     create_time = models.DateTimeField(auto_now_add=True)
