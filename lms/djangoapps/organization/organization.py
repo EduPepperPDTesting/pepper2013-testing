25bc3d5d99e3741e064435a652fb0b980c8ff56f
Scott Sun <495842314@qq.com>
2017-10-10 10:37:26 +0800

    change wrong word and profile

--- lms/djangoapps/organization/organization.py
+++ lms/djangoapps/organization/organization.py
@@ -383,7 +383,7 @@ def organization_add(request):
             organization.save()
 
             # --------------OrganizationDataitems
-            if not oid:
+            if oid == "-1":
                 dataitems = '['
                 dataitems = dataitems + '{"name":"Major Subject Area","required":"1","default":"1"},'
                 dataitems = dataitems + '{"name":"Grade Level-Check all that apply","required":"1","default":"2"},'
