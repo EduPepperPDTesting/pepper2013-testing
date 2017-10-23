007df68ec841663811e02ca63a49d9692e4d3951
Scott Sun <495842314@qq.com>
2017-10-23 13:40:30 +0800

    debug for organization add

--- lms/djangoapps/organization/organization.py
+++ lms/djangoapps/organization/organization.py
@@ -384,7 +384,7 @@ def organization_add(request):
             organization.save()
 
             # --------------OrganizationDataitems
-            if oid == "-1":
+            if oid == "-1" and copyfromid == "":
                 dataitems = '['
                 dataitems = dataitems + '{"name":"Major Subject Area","required":"1","default":"1"},'
                 dataitems = dataitems + '{"name":"Grade Level-Check all that apply","required":"1","default":"2"},'
