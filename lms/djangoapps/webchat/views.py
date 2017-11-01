8f5875c921eb3c827b49d53bea28db17d72611bb
Anatoliy <anatoliy.kogan@gmail.com>
2017-11-01 19:04:25 -0400

    checking if user is in network test

--- lms/djangoapps/webchat/views.py
+++ lms/djangoapps/webchat/views.py
@@ -248,7 +248,7 @@ def get_community_user_rows(request):
 
     my_network = list()
     getMyPeople = my_people(request, checkInNetwork = 1)
-    raise Exception("getMyPeople: "+getMyPeople)
+    raise Exception(getMyPeople)
     myPeopleList = getMyPeople.replace("[{", "[").replace("}]", "]").split("}, {")
 
     myPeopleDict = {}
