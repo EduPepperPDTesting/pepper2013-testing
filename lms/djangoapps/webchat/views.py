53261089f42b4b86cd47608513204e46cf08a17f
Anatoliy <anatoliy.kogan@gmail.com>
2017-11-03 11:09:22 -0400

    debugging chat people search

--- lms/djangoapps/webchat/views.py
+++ lms/djangoapps/webchat/views.py
@@ -51,6 +51,7 @@ def get_all_ptusers(request):
     rows = list()
 
     readMyPeople = my_people(request, checkInNetwork = 1).content
+    raise Exception(readMyPeople)
     getMyPeople = json.loads(readMyPeople)
     my_network_ids = [d["user_id"].encode("utf-8") for d in getMyPeople if 'user_id' in d]
 
