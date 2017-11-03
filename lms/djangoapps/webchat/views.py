65d9f178cd4695215cfa50a57e09aade467dcdd3
Anatoliy <anatoliy.kogan@gmail.com>
2017-11-03 17:31:40 -0400

    peptalk users add/remove update

--- lms/djangoapps/webchat/views.py
+++ lms/djangoapps/webchat/views.py
@@ -32,7 +32,7 @@ def gettextframe(request, uname):
     #     user_name.extend([add_name])
     #     get_name = get_name[space_pos+1:]
 
-    name_index = uname.index("~")
+    name_index = uname.index(";")
     user_class = uname[0: name_index]
     id_index = uname.index("`")
     user_name = uname[name_index+1:id_index].replace("_", " ")
