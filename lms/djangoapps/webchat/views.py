183b6a2e475d4d75d3f90787b9865811bbacf349
Anatoliy <anatoliy.kogan@gmail.com>
2017-11-05 16:46:01 -0500

    user list in user search test

--- lms/djangoapps/webchat/views.py
+++ lms/djangoapps/webchat/views.py
@@ -62,7 +62,7 @@ def get_all_ptusers(request):
         prevLen = len(my_network_ids)
         pageAttr = pageAttr + 1
         getMyPeople = my_people(request, checkInNetwork=1, pageAttr=str(pageAttr)).content
-        my_network_ids = my_network_ids + [d["user_id"].encode("utf-8") for d in getMyPeople if 'user_id' in d]
+        my_network_ids.extend([d["user_id"].encode("utf-8") for d in getMyPeople if 'user_id' in d])
     # pageAttr = 1
     # getMyPeople = my_people(request, checkInNetwork=1, pageAttr=str(pageAttr)).content
     # my_network_ids = [d["user_id"].encode("utf-8") for d in getMyPeople if 'user_id' in d]
