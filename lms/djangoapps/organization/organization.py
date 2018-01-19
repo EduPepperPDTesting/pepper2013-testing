d8a59fd33103a33f20b02e4bcd8b1fb3546a42ef
Scott <495842314@qq.com>
2018-01-19 10:28:31 +0800

    debug for org

--- lms/djangoapps/organization/organization.py
+++ lms/djangoapps/organization/organization.py
@@ -129,8 +129,15 @@ def main(request):
         elif post_flag == "organization_register_save":
             return organization_register_save(request)
     else:
-        tmp = "organization/organization.html"
-        return render_to_response(tmp)
+        if request.user.is_superuser:
+            tmp = "organization/organization.html"
+            return render_to_response(tmp)
+        else:
+            error_context = {'window_title': '403 Error - Access Denied',
+                         'error_title': '',
+                         'error_message': 'You do not have access to this view in Pepper,\
+                          please contact support for any questions at <a href="mailto:pepperpdhelpdesk@pcgus.com">pepperpdhelpdesk@pcgus.com</a>.'}
+            return HttpResponseForbidden(render_to_response('error.html', error_context))
 
 
 # -------------------------------------------------------------------organization_register_save
