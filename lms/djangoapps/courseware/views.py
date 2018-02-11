6320716a66ebfb2c345c60b8f721e5496a685c96
Scott <495842314@qq.com>
2018-02-11 16:05:04 +0800

    debug title

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -76,11 +76,13 @@ def courses(request):
     courses = get_courses(request.user, request.META.get('HTTP_HOST'))
     courses = sort_by_custom(courses)
     extitles = []
+    exurl = []
     titles = ['Mathematics','English Language Arts', 'Science', 'Special Education' ,'Writing and Poetry', 'Leadership', 'English Language Learners', "Pepper's Online Workshops", "Visual & Perfoming Arts", 'Digital Citizenship', "Teacher ToolKit", "State Content Collections", "District Content Collections"]
     for tmp in courses:
         if tmp.create_toplevel_title:
             if (tmp.create_toplevel_title not in titles) and (tmp.create_toplevel_title not in extitles):
                 extitles.append(tmp.create_toplevel_title)
+                exurl.append(tmp.create_toplevel_title.strip().replace(' ','%20'))
 
     state_list, district_list, all_state, all_district = get_state_and_district_list(request, courses)
     #20160324 modify
@@ -95,7 +97,8 @@ def courses(request):
                               "states_keys": state_key_list,
                               "districts_keys": district_key_list,
                               "collections": collections,
-                              "extitles": extitles,
+                              "extitles":extitles,
+                              "exurl":exurl,
                               "link": True})
 
 
