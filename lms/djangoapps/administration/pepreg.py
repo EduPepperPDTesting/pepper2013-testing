8e14c52562b0677a62cd555b8cf8b7828900b6e6
Anatoliy <anatoliy.kogan@gmail.com>
2018-02-27 18:34:38 -0500

    debug time condition

--- lms/djangoapps/administration/pepreg.py
+++ lms/djangoapps/administration/pepreg.py
@@ -1045,7 +1045,7 @@ def build_screen_rows(request, year, month, catype, all_occurrences, current_day
                                         endHour = 0
                                         startHour = int(day[3][i][:day[3][i].index(":")])
                                         startHourAMPM = day[3][i][-2:]
-                                        raise Exception("startHour=" + str(startHour) + "startHourAMPM" + startHourAMPM)
+                                        raise Exception("startHour=" + str(startHour) + "startHourAMPM" + str(startHourAMPM))
                                         if ((startHourAMPM == "AM" and (startHour < 7 or startHour == 12)) or (startHourAMPM == "PM" and startHour > 8)):
                                             if (startHourAMPM == "PM" and startHour == 12):
                                                 startHour = 0
