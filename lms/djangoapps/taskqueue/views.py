64a764e30193bf073d90b90c414a40be71c3a7ab
mike <mike.a.minnick@gmail.com>
2018-03-04 23:28:23 -0500

    Faulty PepConn Import Removed

--- lms/djangoapps/taskqueue/views.py
+++ lms/djangoapps/taskqueue/views.py
@@ -9,7 +9,6 @@ from student.models import User, UserProfile, Registration
 from mitxmako.shortcuts import render_to_string
 from models import Job, Tasks
 from django import db
-from administration.pepconn import render_from_string
 from mail import send_html_mail
 try:
     from urllib import urlencode
@@ -57,16 +56,16 @@ def run_registration_email(task):
         props = {'key': reg.activation_key, 'district': user.profile.district.name, 'email': user.email}
 
         use_custom = email_json.custom_email
+        subject = ""
+        body = ""
         if use_custom == 'true':
             custom_email = email_json.custom_email_body
             custom_email_subject = email_json.custom_email_subject
-            subject = render_from_string(custom_email_subject, props)
-            body = render_from_string(custom_email, props)
         else:
             subject = render_to_string ('emails/acivation_email_subject.txt', props)
             body = render_to_string('emails/activation_email.txt', props)
         subject = ''.join(subject.splitLines())
-        send_html_mail (subject, body, settings.SUPPORT_EMAIL, [user.email])
+        send_html_mail(subject, body, settings.SUPPORT_EMAIL, [user.email])
 
         job.completed = job.completed + 1
 
