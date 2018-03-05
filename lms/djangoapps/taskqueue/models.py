747762457ac0f893e59ceafa066defb773d90837
mike <mike.a.minnick@gmail.com>
2018-03-04 23:10:14 -0500

    TaskQueue Models Update

--- lms/djangoapps/taskqueue/models.py
+++ lms/djangoapps/taskqueue/models.py
@@ -6,3 +6,12 @@ class Tasks(models.Model):
         db_table = 'queue_task'
     data = models.TextField(blank=False, max_length=255)
     function = models.TextField(blank=False, max_length=255)
+    job = models.ForeignKey(Job, on_delete=models.PROTECT)
+
+
+class Job(models.Model):
+    class Meta:
+        db_table = 'queue_table'
+    function = models.TextField(blank=False, max_length=255)
+    completed = models.IntegerField(blank=False, default=0)
+    total = models.IntegerField(blank=False, default=0)
