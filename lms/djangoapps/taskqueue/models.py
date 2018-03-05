caeba45ce532bcc57beecf3539960ea9fa612209
mike <mike.a.minnick@gmail.com>
2018-03-04 23:16:07 -0500

    TaskQueue Models Update - Jobs Table

--- lms/djangoapps/taskqueue/models.py
+++ lms/djangoapps/taskqueue/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 
+class Job(models.Model):
+    class Meta:
+        db_table = 'queue_job'
+    function = models.TextField(blank=False, max_length=255)
+    completed = models.IntegerField(blank=False, default=0)
+    total = models.IntegerField(blank=False, default=0)
+
+
 class Tasks(models.Model):
     class Meta:
         db_table = 'queue_task'
     data = models.TextField(blank=False, max_length=255)
     function = models.TextField(blank=False, max_length=255)
-    job = models.ForeignKey(Job, on_delete=models.PROTECT)
 
 
-class Job(models.Model):
-    class Meta:
-        db_table = 'queue_table'
-    function = models.TextField(blank=False, max_length=255)
-    completed = models.IntegerField(blank=False, default=0)
-    total = models.IntegerField(blank=False, default=0)
