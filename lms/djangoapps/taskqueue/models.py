477dc0134dc6ef7f56370d5eb5945ae84b640104
mike <mike.a.minnick@gmail.com>
2018-03-04 23:17:26 -0500

    TaskQueue Models Update - Jobs Key

--- lms/djangoapps/taskqueue/models.py
+++ lms/djangoapps/taskqueue/models.py
@@ -14,5 +14,6 @@ class Tasks(models.Model):
         db_table = 'queue_task'
     data = models.TextField(blank=False, max_length=255)
     function = models.TextField(blank=False, max_length=255)
+    job = models.ForeignKey(Job, on_delete=models.PROTECT)
 
 
