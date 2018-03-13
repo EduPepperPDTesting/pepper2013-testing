86759ab7018a535ae78fa4a9406b8b7cf832ce72
Anatoliy <anatoliy.kogan@gmail.com>
2018-03-13 17:27:34 -0400

    registering top student from waitlist edit

--- lms/djangoapps/administration/pepreg.py
+++ lms/djangoapps/administration/pepreg.py
@@ -1521,7 +1521,6 @@ def delete_student(request):
         on_waitlist = PepRegStudent.objects.filter(training_id=training_id, student_status='Waitlist')
         if training.allow_waitlist and on_waitlist.count > 0:
             top_on_waitlist = on_waitlist.values().order_by('id')[:1]
-            raise Exception("top_on_waitlist="+str(top_on_waitlist[0]['student_id']))
             register(request, top_on_waitlist[0]['student_id'])
 
     except Exception as e:
