45b026018c623aae84afc89a5d21a127ac2e57d3
Anatoliy <anatoliy.kogan@gmail.com>
2018-03-14 16:49:16 -0400

    updated reach_limit function

--- lms/djangoapps/administration/pepreg.py
+++ lms/djangoapps/administration/pepreg.py
@@ -153,7 +153,7 @@ def build_sorts(columns, sorts):
 
 def reach_limit(training):
     return training.max_registration > 0 and PepRegStudent.objects.filter(
-        training=training).count() >= training.max_registration
+        training=training, student_status!='Waitlist').count() >= training.max_registration
 
 
 def instructor_names(training):
