b29150a8cb819810601ad03a6193b97520052c48
Anatoliy <anatoliy.kogan@gmail.com>
2018-03-14 16:27:11 -0400

    debug deleting student

--- lms/djangoapps/administration/pepreg.py
+++ lms/djangoapps/administration/pepreg.py
@@ -1282,8 +1282,8 @@ def register_student(request, join, training_id, user_id):
                 mem.delete()
 
             on_waitlist = PepRegStudent.objects.filter(training_id=training_id, student_status='Waitlist')
-            if training.allow_waitlist and on_waitlist.count > 0:
-                raise Exception('no waitlist')
+            if training.allow_waitlist and on_waitlist.count() > 0:
+                #raise Exception('no waitlist')
                 top_on_waitlist = on_waitlist.values().order_by('id')[:1][0]['student_id']
                 register_student(request, True, training_id, top_on_waitlist)
 
@@ -1537,10 +1537,10 @@ def delete_student(request):
 
         training = PepRegTraining.objects.get(id=training_id)
         on_waitlist = PepRegStudent.objects.filter(training_id=training_id, student_status='Waitlist')
-        if training.allow_waitlist and on_waitlist.count > 0:
+        if training.allow_waitlist and on_waitlist.count() > 0:
             top_on_waitlist = on_waitlist.values().order_by('id')[:1][0]['student_id']
             register_data = register_student(request, True, training_id, top_on_waitlist)
-            raise Exception(str(register_data[0])+" "+str(register_data[1]))
+            #raise Exception(str(register_data[0])+" "+str(register_data[1]))
             if register_data[0] == False:
                 return HttpResponse(json.dumps({'success': False, 'error': '%s' % register_data[1]}), content_type="application/json")
 
