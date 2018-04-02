2e4fe8aaae7e20dfbd97b5091c247fc4336c00f1
czy <zhiyuc@outlook.com>
2018-04-02 11:10:07 +0800

    revert and fix

--- lms/djangoapps/playbook/views.py
+++ lms/djangoapps/playbook/views.py
@@ -92,9 +92,6 @@ def get_play_list(request):
         data['step_nums'] = 0
         data['category_name'] = play.category.name
         data['created_time'] = play.created_time
-        log.debug("===================================================")
-        log.debug(type(play.created_time))
-        log.debug(play.created_time)
         data['creator'] = play.creator.profile.getFullname();
         play_list_fragment += render_to_string('playbook/play-list-in-tenant-fragment.html', data)
 
