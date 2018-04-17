6351db509d56e609807648cddcbb759aabfb88e2
Anatoliy <anatoliy.kogan@gmail.com>
2018-04-17 12:12:45 -0400

    remove course fields

--- lms/djangoapps/administration/models.py
+++ lms/djangoapps/administration/models.py
@@ -264,8 +264,6 @@ class PepRegStudentCourse(models.Model):
     class Meta:
         db_table = 'pepregstudent_course'
     training = models.ForeignKey(PepRegTraining)
-    course_id = models.IntegerField(blank=False, default=0)
-    course = models.ForeignKey(CourseEnrollment)
     student = models.ForeignKey(User, related_name='+')
     user_create = models.ForeignKey(User, related_name='+')
     date_create = models.DateField(auto_now_add=True, db_index=False)
